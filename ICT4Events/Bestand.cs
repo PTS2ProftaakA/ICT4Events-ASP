1b437895829b9ae20f1155f729e09c4aa027398c
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2015-05-27 15:37:51 +0200

    De klassen Bericht, Bestand, Bijdrage en Categorie toegevoegd.

---
+++ ICT4Events/Bestand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICT4Events
+{
+    public class Bestand : Bijdrage
+    {
+        //Fields
+        private string bestandslocatie;
+        private int grootte;
+
+        //Properties
+        #region properties
+        public string Bestandslocatie
+        {
+            get { return bestandslocatie; }
+            set { bestandslocatie = value; }
+        }
+
+        public int Grootte
+        {
+            get { return grootte; }
+            set { grootte = value; }
+        }
+        #endregion
+
+        //Constructor
+        public Bestand(int id, DateTime datum, BijdrageType soort, string bestandslocatie, int grootte)
+            : base(id, datum, soort)
+        {
+            this.bestandslocatie = bestandslocatie;
+            this.grootte = grootte;
+        }
+    }
+}
