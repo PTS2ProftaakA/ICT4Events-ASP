7244f9994fa404e4d47e97b3ad1210b5b9cbdae1
Boon,Matthijs M. van der <m.vanderboon@student.nl>
2015-05-27 14:36:35 +0200

    Het eerste project geupload

---
+++ ICT4Events/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICT4Events.Startup))]
+namespace ICT4Events
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
